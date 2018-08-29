4dbc797726545c54515ff47ebc89f899723e3648
Nebres <serbenski@o2.pl>
2018-08-29 19:52:02 +0200

    zadanie14_3 | dodadnie setterów i getterów, dodanie Equal/Hashcode oraz ToString

--- src/main/java/com/crud/tasks/domain/TaskDto.java
+++ src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -1,7 +1,8 @@
-package com.crud.tasks.com.crud.tasks.domain;
+package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import java.util.Objects;
 
 @Getter
 @AllArgsConstructor
@@ -10,5 +11,29 @@ public class TaskDto {
     private Long id;
     private String title;
     private String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TaskDto)) return false;
+        TaskDto taskDto = (TaskDto) o;
+        return Objects.equals(getId(), taskDto.getId()) &&
+                Objects.equals(getTitle(), taskDto.getTitle()) &&
+                Objects.equals(getContent(), taskDto.getContent());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle(), getContent());
+    }
+
+    @Override
+    public String toString() {
+        return "TaskDto{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 
 }

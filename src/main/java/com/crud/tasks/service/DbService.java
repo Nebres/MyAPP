f788d311571d99aa47211e03b171a345a6c89378
Nebres <serbenski@o2.pl>
2018-09-01 20:18:35 +0200

    zadanie15_2 | korekta properties

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -2,12 +2,14 @@ package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@NoArgsConstructor
 public class DbService {
 
     @Autowired

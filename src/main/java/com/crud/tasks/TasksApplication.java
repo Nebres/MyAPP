b6046759b50a39b4916b999f06ff7b35d66ff190
Nebres <serbenski@o2.pl>
2018-09-02 16:53:31 +0200

    master  | refaktoryzacja

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -3,7 +3,6 @@ package com.crud.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class TasksApplication {
 

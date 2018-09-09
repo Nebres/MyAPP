7ca7b970567a476ee13fcb320c45c79140333aef
Nebres <serbenski@o2.pl>
2018-09-09 17:26:25 +0200

    zadanie 17_4 | utworzenie skryptu wywołujacego getTasks

---
+++ showtasks.bat
@@ -0,0 +1,13 @@
+call C:\Users\Przemek\IdeaProjects\tasks\runcrud.bat
+if "%ERRORLEVEL%" == "0" goto runbrowser
+.echo
+echo Fail Cannot open runcrud.bat
+
+:runbrowser
+start http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "0" goto end
+.echo
+echo Fail Cannot open Firefox
+
+:end
+echo END
